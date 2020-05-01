97217633719ab3cfe1e9a67ccdd849b3f03545e1
John Knowles <jtknowles007@gmail.com>
2020-04-30 21:04:31 -0400

    Use wget and PySimpleGUI to display comic

--- polybar/.config/polybar/scripts/xkcdshow.py
+++ polybar/.config/polybar/scripts/xkcdshow.py
@@ -1,8 +1,21 @@
 #! /usr/bin/env python3
 
-import webbrowser
+import PySimpleGUI as sg
+import wget
 
-xkcdfile = open('xkcd.txt','r')
-url = xkcdfile.read()
-xkcdfile.close()
-webbrowser.open(url)
+import polymodule as pm
+
+url = 'https://xkcd.com/info.0.json'
+xkcd = pm.getjson(url)
+image = xkcd['img']
+
+imagefile = wget.download(image)
+sg.theme('DarkAmber')
+layout = [[sg.Image(imagefile)],[sg.Button('Close')]]
+window = sg.Window('xkcd', layout, location=(0,0))
+
+while True:
+    event, values = window.read()
+    if event in (None,'Close'):
+        break
+window.close()
