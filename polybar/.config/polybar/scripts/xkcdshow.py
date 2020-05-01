594317e0b9b1e901cfb3cc340533bf35b03f3a50
John Knowles <jtknowles007@gmail.com>
2020-05-01 12:39:15 -0400

    Comment to reference i3 window location

--- polybar/.config/polybar/scripts/xkcdshow.py
+++ polybar/.config/polybar/scripts/xkcdshow.py
@@ -12,6 +12,8 @@ image = xkcd['img']
 imagefile = wget.download(image)
 sg.theme('DarkAmber')
 layout = [[sg.Image(imagefile)],[sg.Button('Close')]]
+
+# To specify exact monitor where this window should be place, see i3 config file.
 window = sg.Window('xkcd_comic', layout, no_titlebar=True, location=(700,32))
 
 while True:
