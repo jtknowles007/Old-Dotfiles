a1f85896dc58488cf97cfc67af51707823442e91
John Knowles <jtknowles007@gmail.com>
2020-05-06 10:31:41 -0400

    Text padding

--- polybar/.config/polybar/scripts/mpc.sh
+++ polybar/.config/polybar/scripts/mpc.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 if ! mpc >/dev/null 2>&1; then
-    echo MPC Offline
+    echo 'MPC Offline           ' 
     exit 1
 elif mpc status | grep -q playing; then
     (mpc --format "%title%  %album%" current | zscroll -l 20 -b " " -p "  " --delay 0.3 --update-check true "mpc --format '%title%  %album%' current") &
 else
-    echo  Not Playing
+    echo ' Not Playing         ' 
 fi
 mpc idle >/dev/null
