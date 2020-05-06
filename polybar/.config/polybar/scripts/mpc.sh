d8aeaf099afe25578ba9e46eb137f586c3132c6e
John Knowles <jtknowles007@gmail.com>
2020-05-06 09:39:58 -0400

    MPD output formatting

--- polybar/.config/polybar/scripts/mpc.sh
+++ polybar/.config/polybar/scripts/mpc.sh
@@ -3,8 +3,8 @@ if ! mpc >/dev/null 2>&1; then
     echo MPC Offline
     exit 1
 elif mpc status | grep -q playing; then
-    (mpc --format "%album% ** %title%" current | zscroll -l 20 -b " " -p " ** " --delay 0.3 --update-check true "mpc --format '%album% ** %title%' current ") &
+    (mpc --format "%title%  %album%" current | zscroll -l 20 -b " " -p "  " --delay 0.3 --update-check true "mpc --format '%title%  %album%' current") &
 else
-    echo Not Playing
+    echo  Not Playing
 fi
 mpc idle >/dev/null
