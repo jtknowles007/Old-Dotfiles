52f4dd3e788e71102be60b37d0a43122e13c56c6
John Knowles <jtknowles007@gmail.com>
2020-08-05 11:52:16 -0400

    Updated API call

--- polybar/.config/polybar/scripts/covid.py
+++ polybar/.config/polybar/scripts/covid.py
@@ -6,6 +6,7 @@
 import json
 import requests
 import polymodule as pm
+from recursive_json import extract_values
 
 #Assign Pywal colors
 virusiconcolor = pm.pycolors('color6')
@@ -13,12 +14,15 @@ positivecolor = pm.pycolors('color13')
 deathscolor = pm.pycolors('color10')
 
 #Gather COVID-9 Data
-covidurl = "https://covid19api.io/api/v1/CasesInAllUSStates"
+covidurl = "https://api.covid19api.com/summary"
 covid = requests.get(covidurl)
 coviddata = json.loads(covid.text)
 
-totalcases = coviddata['data'][0]['table'][0]['TotalCases']
-totaldeaths = coviddata['data'][0]['table'][0]['TotalDeaths']
+country = extract_values(coviddata,'CountryCode')
+cases = extract_values(coviddata,'TotalConfirmed')
+deaths = extract_values(coviddata,'TotalDeaths')
+uscases = cases[-9]
+usdeaths = deaths[-9]
 
 #Display COVID-19 data in Polybar
-print("%{{F{} T4}}若%{{F- T-}} US: %{{F{}}} %{{F-}}{} %{{F{}}} %{{F-}} {}".format(virusiconcolor,positivecolor,totalcases,deathscolor,totaldeaths))
+print("%{{F{} T4}}若%{{F- T-}} US: %{{F{}}} %{{F-}}{:,} %{{F{}}} %{{F-}}{:,}".format(virusiconcolor,positivecolor,uscases,deathscolor,usdeaths))
