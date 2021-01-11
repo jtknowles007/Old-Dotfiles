04e24d78380c6f3aab71c4bf5c94f95910cd318b
John Knowles <jtknowles007@gmail.com>
2021-01-11 16:13:23 -0500

    Revised monitor names for Nvidia drivers

--- polybar/.config/polybar/scripts/launch.sh
+++ polybar/.config/polybar/scripts/launch.sh
@@ -5,7 +5,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR=DVI-I-1 polybar primary_bar &
-MONITOR=HDMI-1 polybar secondary_bar &
+MONITOR=DVI-I-0 polybar primary_bar &
+MONITOR=HDMI-0 polybar secondary_bar &
 
 
