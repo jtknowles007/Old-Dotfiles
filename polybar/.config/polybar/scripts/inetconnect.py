10ab86fbcf00d84c1ecd34b722f9c70e0fb5a740
John Knowles <jtknowles007@gmail.com>
2021-03-17 16:04:11 -0400

    Revised text display

--- polybar/.config/polybar/scripts/inetconnect.py
+++ polybar/.config/polybar/scripts/inetconnect.py
@@ -2,15 +2,21 @@
 
 
 import urllib.request
+import polymodule as pm
 
 theurl = "http://www.msftncsi.com/ncsi.txt"
+theupicon = ""
+thedownicon = ""
+theupcolor = pm.pycolors('color6')
+thedowncolor = pm.pycolors('color2')
+
 therequest = urllib.request.Request(theurl)
 try:
     theresponse = urllib.request.urlopen(therequest)
-    print("")
+    print("%{{F{}}} {}%{{F-}}".format(theupcolor,theupicon))
 except urllib.error.HTTPError as e:
-    print("")
+    print("%{{F{}}} {}%{{F-}}".format(thedowncolor,thedownicon))
 except urllib.error.URLError as e:
-    print("")
+    print("%{{F{}}} {}%{{F-}}".format(thedowncolor,thedownicon))
 
 
