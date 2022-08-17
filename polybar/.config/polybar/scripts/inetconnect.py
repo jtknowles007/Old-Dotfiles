04546ba0181e2a80d7d0b7432d7c9f22d31ff7da
John Knowles <jtknowles007@gmail.com>
2022-08-17 10:38:02 -0400

    Updated weather script for use with Conky

--- polybar/.config/polybar/scripts/inetconnect.py
+++ polybar/.config/polybar/scripts/inetconnect.py
@@ -1,21 +1,30 @@
 #! /usr/bin/env python3
 
 import urllib.request
-import polymodule as pm
+#import polymodule as pm
 
 theurl = "http://www.msftncsi.com/ncsi.txt"
-theupicon = " "
-thedownicon = " "
-theupcolor = pm.pycolors('color7')
-thedowncolor = pm.pycolors('color2')
+#  Old Polybar Version
+#theupicon = " "
+#thedownicon = " "
+#theupcolor = pm.pycolors('color7')
+#thedowncolor = pm.pycolors('color2')
+
+# New Conky Version
+theupicon = "${font Font Awesome 6 Free Solid:size=11}${color 009900} ${color 4F4F4F}${font} "
+thedownicon = "$font Font Awesome 6 Free Solid:size=11}${color 990000} ${color 4f4f4f}{$font}"
+
 
 therequest = urllib.request.Request(theurl)
 try:
     theresponse = urllib.request.urlopen(therequest)
-    print("%{{F{}}} {}%{{F-}}".format(theupcolor,theupicon))
+    #print("%{{F{}}} {}%{{F-}}".format(theupcolor,theupicon))
+    print(theupicon)
 except urllib.error.HTTPError as e:
-    print("%{{F{}}} {}%{{F-}}".format(thedowncolor,thedownicon))
+    #print("%{{F{}}} {}%{{F-}}".format(thedowncolor,thedownicon))
+    print(thedownicon)
 except urllib.error.URLError as e:
-    print("%{{F{}}} {}%{{F-}}".format(thedowncolor,thedownicon))
+    #print("%{{F{}}} {}%{{F-}}".format(thedowncolor,thedownicon))
+    print(thedownicon)
 
 
