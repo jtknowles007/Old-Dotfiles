9930f115c30f7c48e6bad7b83c97d182eec95218
John Knowles <jtknowles007@gmail.com>
2022-08-30 11:41:07 -0400

    Initial Commit

---
+++ polybar/.config/polybar/scripts/ticker2.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+from datetime import datetime, time
+import yfinance as yf
+def stock(symbol):
+    lookup = yf.Ticker(symbol)
+    quoteprice = lookup.info['regularMarketPrice']
+    openprice = lookup.info['previousClose']
+    if quoteprice >=1:
+        currentprice = round(quoteprice)
+        diffprice = abs(round(quoteprice - openprice))
+    else:
+        currentprice = round(quoteprice,3)
+        diffprice = round((quoteprice - openprice,3))
+    if diffprice >0:
+        symbol = '${color 009900}  ${color1}'
+    elif diffprice <0:
+        symbol = '${color 990000}  ${color1}'
+    else:
+        symbol = ' '
+    return [currentprice,symbol,diffprice]
+
+def main():
+    stocklist = ['^DJI','^IXIC','^GSPC']
+    dji = stock(stocklist[0])
+    ndq = stock(stocklist[1])
+    sp5 = stock(stocklist[2])
+
+    print("DJIA:${{goto 150}}{:,}${{font Font Awesome 6 Free Solid:size=20}}${{goto 245}}{}${{font}}${{voffset -1}}{:,}".format(dji[0],dji[1],dji[2]))
+    print("NASDAQ:${{goto 150}}{:,}${{font Font Awesome 6 Free Solid:size=20}}${{goto 245}}{}${{font}}${{voffset -1}}{:,}".format(ndq[0],ndq[1],ndq[2]))
+    print("S&P 500:${{goto 150}}{:,}${{font Font Awesome 6 Free Solid:size=20}}${{goto 245}}{}${{font}}${{voffset -1}}{:,}".format(sp5[0],sp5[1],sp5[2]))
+
+if __name__ == "__main__":
+    main()
