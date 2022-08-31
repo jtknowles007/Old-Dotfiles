e6d93f2ecc45496eb06902c3eb8039bb46cc09f3
John Knowles <jtknowles007@gmail.com>
2022-08-30 21:59:06 -0400

    Update to ticker

--- polybar/.config/polybar/scripts/ticker2.py
+++ polybar/.config/polybar/scripts/ticker2.py
@@ -30,5 +30,14 @@ def main():
     print("NASDAQ:${{goto 150}}{:,}${{font Font Awesome 6 Free Solid:size=20}}${{goto 245}}{}${{font}}${{voffset -1}}{:,}".format(ndq[0],ndq[1],ndq[2]))
     print("S&P 500:${{goto 150}}{:,}${{font Font Awesome 6 Free Solid:size=20}}${{goto 245}}{}${{font}}${{voffset -1}}{:,}".format(sp5[0],sp5[1],sp5[2]))
 
+    header = "NAME\tVALUE\tCHANGE\n"
+    dji_out = "DJIA\t{:,}\t{:+g}\n".format(dji[0],dji[2])
+    ndq_out = "NASDAQ\t{:,}\t{:+g}\n".format(ndq[0],ndq[2])
+    sp5_out = "S&P 500\t{:,}\t {:+g}".format(sp5[0],sp5[2])
+
+    file = open("output.txt", "w")
+    file.write(header + dji_out + ndq_out + sp5_out)
+    file.close
+
 if __name__ == "__main__":
     main()
