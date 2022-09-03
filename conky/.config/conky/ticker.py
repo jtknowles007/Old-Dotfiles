0fb369adf561624a88b9efaf262460bf0d7f76d3
John Knowles <jtknowles007@gmail.com>
2022-09-03 14:16:11 -0400

    Add weekday logic. Add write to txt file.

--- conky/.config/conky/ticker.py
+++ conky/.config/conky/ticker.py
@@ -21,7 +21,7 @@ def stock(symbol):
     elif diffprice <0:
         symbol = '${color 990000}  '
     else:
-        symbol = ' '
+        symbol = '${color 333333}  '
     return [currentprice,symbol,diffprice]
 
 def getstocks():
@@ -34,19 +34,31 @@ def getstocks():
     print("${{color}}NASDAQ:${{goto 160}}{:,}${{font Font Awesome 6 Free Solid:size=10}}${{goto 235}}{}${{font}}${{color}}${{voffset -1}}${{alignr}}{:,}".format(ndq[0],ndq[1],ndq[2]))
     print("${{color}}S&P 500:${{goto 160}}{:,}${{font Font Awesome 6 Free Solid:size=10}}${{goto 235}}{}${{font}}${{color}}${{voffset -1}}${{alignr}}{:,}".format(sp5[0],sp5[1],sp5[2]))
 
+    header = "NAME\tVALUE\tCHANGE\n"
+    dji_out = "DJIA\t{:,}\t{:+g}\n".format(dji[0],dji[2])
+    ndq_out = "NASDAQ\t{:,}\t{:+g}\n".format(ndq[0],ndq[2])
+    sp5_out = "S&P 500\t{:,}\t{:+g}".format(sp5[0],sp5[2])
+
+    file = open("output.txt","w")
+    file.write(header + dji_out + ndq_out + sp5_out)
+    file.close
+
 def offhours():
     print("Markets Closed")
     with open("/home/john/.config/conky/output.txt", "r") as stocks:
         tsv_reader = csv.DictReader(stocks, delimiter="\t")
         for stock_index in tsv_reader:
             print("${{color6}}{}${{goto 170}}{}${{goto 245}}${{alignr}}{}${{color}}".format(stock_index["NAME"],stock_index["VALUE"],stock_index["CHANGE"]))
+
 def main():
+    date_now = datetime.datetime.now()
+    day_number = date_now.weekday()
     time_now = datetime.datetime.now().time()
     time_open = datetime.time(9,30,0)
     time_close = datetime.time(16,0,0)
-
-    if time_now >= time_open and time_now <= time_close:
-        getstocks()
+    if day_number <5:
+        if time_now >= time_open and time_now <= time_close:
+            getstocks()
     else:
         offhours()
 
