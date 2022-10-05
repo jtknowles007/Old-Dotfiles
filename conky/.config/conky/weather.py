665d120cd0dcdeaf0e629fe009986af34355fce0
John Knowles <jtknowles007@gmail.com>
2022-10-05 16:24:44 -0400

    Remove Hi Lo / add humidity

--- conky/.config/conky/weather.py
+++ conky/.config/conky/weather.py
@@ -7,6 +7,7 @@ from credentials import openweather
 latitude = 40.05 # Lat of Anderson IN
 longitude = -85.67 # Lon of Anderson IN
 symbol = "°F"
+symbol2 = "%"
 path = "~/Dotfiles/conky/.config/conky/weatherimages/"
 filetype = ".png"
 
@@ -17,8 +18,8 @@ weatherdata = pm.getjson(weatherurl)
 
 # Gather data elements from weather report that we're interested in displaying
 current = round(weatherdata['main']['temp'],0)
-high = round(weatherdata['main']['temp_max'],0)
-low = round(weatherdata['main']['temp_min'],0)
+feelslike = round(weatherdata['main']['feels_like'],0)
+humidity = round(weatherdata['main']['humidity'],0)
 condition = weatherdata['weather'][0]['description'].title()
 icon = weatherdata['weather'][0]['icon']
 
@@ -27,5 +28,5 @@ iconpath = path + icon + filetype
 
 # Output weather in Conky format
 print("${{image {} -p 0,0 -s 50x50}} ${{offset 50}}${{voffset 20}}{}${{offset 25}}" \
-      "Currently:{}{} ${{offset 25}}Low:{}{}${{offset 10}}High:{}{}" \
-      .format(iconpath,condition,current,symbol,low,symbol,high,symbol))
+      "Currently:{}{} ${{offset 25}}Feels Like:{}{} ${{offset 25}}Humidity:{}{}" \
+      .format(iconpath,condition,current,symbol,feelslike,symbol,humidity,symbol2))
