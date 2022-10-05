fc1e5164a86eaf955968e0eeda81a8d764f42b42
John Knowles <jtknowles007@gmail.com>
2022-10-05 12:53:20 -0400

    Clean up

--- conky/.config/conky/weather.py
+++ conky/.config/conky/weather.py
@@ -3,22 +3,29 @@
 import getjson as pm
 from credentials import openweather
 
-latitude = 40.05
-longitude = -85.67
+# Variables
+latitude = 40.05 # Lat of Anderson IN
+longitude = -85.67 # Lon of Anderson IN
+symbol = "°F"
+path = "~/Dotfiles/conky/.config/conky/weatherimages/"
+filetype = ".png"
 
-# Find local weather
-weatherurl = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=imperial&appid={}".format(latitude,longitude,openweather)
+# Get current weather from OpenWeather and extract JSON data
+weatherurl = ("https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}" \
+              "&units=imperial&appid={}".format(latitude,longitude,openweather))
 weatherdata = pm.getjson(weatherurl)
 
-# Gather data elements from weather report that we're interested in seeing
+# Gather data elements from weather report that we're interested in displaying
 current = round(weatherdata['main']['temp'],0)
 high = round(weatherdata['main']['temp_max'],0)
 low = round(weatherdata['main']['temp_min'],0)
-symbol = "°F"
 condition = weatherdata['weather'][0]['description'].title()
-path = "~/Dotfiles/conky/.config/conky/weatherimages/"
 icon = weatherdata['weather'][0]['icon']
-filetype = ".png"
+
+#Built path to the icons
 iconpath = path + icon + filetype
-# Output Weather 
-print("${{image {} -s 50x50}} ${{offset 50}}${{voffset 20}}{}${{offset 25}}Currently:{}{} ${{offset 25}}Low:{}{}${{offset 10}}High:{}{}".format(iconpath,condition,current,symbol,low,symbol,high,symbol))
+
+# Output weather in Conky format
+print("${{image {} -s 50x50}} ${{offset 50}}${{voffset 20}}{}${{offset 25}}" \
+      "Currently:{}{} ${{offset 25}}Low:{}{}${{offset 10}}High:{}{}" \
+      .format(iconpath,condition,current,symbol,low,symbol,high,symbol))
