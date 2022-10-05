e112f1456ce31af5f37062bbbfb5d6d02703690f
John Knowles <jtknowles007@gmail.com>
2022-10-05 12:59:23 -0400

    Clean up

--- conky/.config/conky/ticker.py
+++ conky/.config/conky/ticker.py
@@ -43,17 +43,23 @@ def getstocks():
             stockresult = stock(list)
 
             #output stock data in conky format
-            print("${{color}}{}${{goto 165}}{}${{goto 245}}${{alignr}}{}{:+g}${{color}}".format(stockresult[0],stockresult[1],stockresult[2],stockresult[3]))
+            print("${{color}}{}${{goto 165}}{}${{goto 245}}${{alignr}}" \
+                  "{}{:+g}${{color}}".format(stockresult[0],stockresult[1], \
+                                             stockresult[2],stockresult[3]))
 
             #output stock data to text file for off hours
-            file.write("{}\t{}\t{:+g}\n".format(stockresult[0],stockresult[1],stockresult[3]))
+            file.write("{}\t{}\t{:+g}\n".format(stockresult[0], \
+                                                stockresult[1],stockresult[3]))
         file.close
 
 def offhours():
     with open("/home/john/.config/conky/output.txt", "r") as stocks:
         tsv_reader = csv.DictReader(stocks, delimiter="\t")
         for stock_index in tsv_reader:
-            print("${{color6}}{}${{goto 160}}{}${{goto 245}}${{alignr}}{}${{color}}".format(stock_index["NAME"],stock_index["VALUE"],stock_index["CHANGE"]))
+            print("${{color6}}{}${{goto 160}}{}${{goto 245}}${{alignr}}{}" \
+                  "${{color}}".format(stock_index["NAME"], \
+                                      stock_index["VALUE"], \
+                                      stock_index["CHANGE"]))
 
 def main():
     date_now = datetime.datetime.now()
@@ -63,8 +69,10 @@ def main():
     time_close = datetime.time(16,0,0)
     us_holidays = holidays.US()
 
-    # Weekday, Non-Holiday, between market open and close?  Run the current stocks.  Otherwise, display closing numbers
-    if day_number <5 and date_now not in us_holidays and time_now >= time_open and time_now <= time_close:
+    # Weekday, Non-Holiday, between market open and close?
+    # Run the current stocks.  Otherwise, display closing numbers
+    if day_number <5 and date_now not in us_holidays and time_now >= \
+       time_open and time_now <= time_close:
         getstocks()
     else:
         offhours()
