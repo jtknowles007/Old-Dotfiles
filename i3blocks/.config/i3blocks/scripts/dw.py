24907aa76cc4b2392091018e901b81df8361109e
John Knowles <jtknowles007@gmail.com>
2023-04-03 12:43:13 -0400

    Darkweather discontinued OpenWeather replacement

--- i3blocks/.config/i3blocks/scripts/dw.py
+++ i3blocks/.config/i3blocks/scripts/dw.py
@@ -1,39 +1,38 @@
 #! /usr/bin/env python3
 
 import getjson as pm
-from credentials import darkkey
+from credentials import openweatherkey
 
-
-# Find Latitude and Longitude based on IP
-ipurl = "https://ipapi.co/json/"
-ipdata = pm.getjson(ipurl)
 latitude = 40.1079
 longitude = -85.6782
 
 # Find local weather
-weatherurl = "https://api.darksky.net/forecast/{}/{},{}".format(darkkey,latitude,longitude)
+weatherurl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat={}&lon={}&appid={}".format(latitude,longitude,openweatherkey)
 weatherdata = pm.getjson(weatherurl)
-
-# Gather data elements from weather report that we're interested in seeing
-current = round(weatherdata['currently']['temperature'],1)
+current = round(weatherdata['main']['temp'],1)
 symbol = "°F"
-condition = weatherdata['currently']['summary']
-
-# Get the weather icon code and match it to the corresponding weather font
-# unicode characters from https://erikflowers.github.io/weather-icons/
-icon = weatherdata['currently']['icon']
+condition = weatherdata['weather'][0]['description']
+icon = weatherdata['weather'][0]['icon']
 icondefault = ""
 icondict = {
-    "clear-day":"",
-    "clear-night":"",
-    "partly-cloudy-day":"",
-    "partly-cloudy-night":"",
-    "rain":"",
-    "sleet":"",
-    "snow":"",
-    "wind":"",
-    "fog":""
-    }
+    "01d":"",
+    "01n":"",
+    "02d":"",
+    "02n":"",
+    "03d":"",
+    "03n":"",
+    "04d":"",
+    "04n":"",
+    "09d":"",
+    "09n":"",
+    "10d":"",
+    "10n":"",
+    "11d":"",
+    "11n":"",
+    "13d":"",
+    "13n":"",
+    "50d":"",
+    "50n":""
 
-# Output Weather 
+}
 print(" {}{} <span font='Weather Icons'>{}</span>".format(current, symbol, icondict.get(icon,icondefault)))
